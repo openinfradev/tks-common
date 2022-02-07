0773480558739449d2a5d32338a4600fc09cfdd5
taekyu.kang <taekyu.kang@sk.com>
2022-02-07 13:42:41 +0900

    Fix. add githuc action.

--- pkg/helper/grpc.go
+++ pkg/helper/grpc.go
@@ -4,16 +4,19 @@ import (
 	"fmt"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 
 	"github.com/openinfradev/tks-common/pkg/log"
 )
 
 func CreateConnection(address string, port int, caller string) (*grpc.ClientConn, error) {
+insecure.NewCredentials()
+
 	host := fmt.Sprintf("%s:%d", address, port)
 	conn, err := grpc.Dial(
 		host,
-		grpc.WithInsecure(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithUnaryInterceptor(
 			grpc_middleware.ChainUnaryClient(
 				log.IOLog(),
